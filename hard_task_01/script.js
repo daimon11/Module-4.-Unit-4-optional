fc3fd433dc638f71c671203c7d6d56c0128d9109
Dmitriy Bezgodko <daimon.94@mail.ru>
2022-06-23 21:15:41 +0300

    23-06-22(21-15)

--- hard_task_01/script.js
+++ hard_task_01/script.js
@@ -1,7 +1,7 @@
 let num1 = +prompt('Введите первое число:', '')
 let num2 = +prompt('Введите второе число:', '')
 
-const gcd_two_numbers = () => {
+const gcd_two_numbers = (num1, num2) => {
   let x = Math.abs(num1);
   let y = Math.abs(num2);
   while(y) {
@@ -11,4 +11,4 @@ const gcd_two_numbers = () => {
   }
   return x;
 }
-console.log(gcd_two_numbers());
+console.log(gcd_two_numbers(num1, num2));
--- hard_task_02/script.js
+++ hard_task_02/script.js
@@ -1,8 +1,8 @@
-const abMin = () => {
+const abMin = (a, b) => {
   let min = (a > b) * b + (b > a) * a;
   return min;
 }
 
 const a = +prompt('Введите первое число: ', '');
 const b = +prompt('Введите второе число: ', '');
-alert(abMin())
+alert(abMin(a, b))
